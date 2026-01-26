6e6b1d070dffcb10b072896b411b837d1d45ea63
PitzonXD <benpitzon@gmail.com>
2026-01-26 17:49:57 +0100

    Sets up initial project structure
    Initializes the project with the Third Person template.
    Configures default project settings, input mappings, and engine settings.
    Adds necessary content files and assets from the template.
    Includes .gitignore entries for IDE-specific files and folders.

---
+++ Source/GameJame2026/GameJame2026.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "GameJame2026.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GameJame2026, "GameJame2026" );
